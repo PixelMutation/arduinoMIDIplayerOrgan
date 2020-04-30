c4f7d78dc146e8dd720181225f8ccfb50772a9a4
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-04-30 18:36:54 +0100

    Added new stop state printing function

--- utility.cpp
+++ utility.cpp
@@ -62,3 +62,21 @@ void printKeyStates(string option) {
 		cout << "\n                        01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 ";
 	} 
 }
+
+void printStopStates(string option) {
+	if (option == "full") {
+		cout << "\n stops user state   : "; Stops.getStatesVector("user", true, true);
+		cout << "\n stops overall state: "; Stops.getStatesVector("all", true, true);
+		cout << "\n stops system state : "; Stops.getStatesVector("system", true, true);
+	} else if (option =="overall") {
+		cout << "\n stops overall state: "; Stops.getStatesVector("all", true, true);
+	} else if (option == "user") {
+		cout << "\n stops user state   : "; Stops.getStatesVector("user", true, true);
+	} else if (option == "system") {
+		cout << "\n stops system state : "; Stops.getStatesVector("system", true, true);
+	} else if (option == "header") {
+		cout << "\n                       01 02 03 04 05 06 07 08 09 10 11";
+	}
+}
+
+
