9f4d189942296aedbd9d7c4b717253d43c015990
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-04-30 18:37:49 +0100

    Added hall sensor reader at the start

--- playerReedOrgan.ino
+++ playerReedOrgan.ino
@@ -9,7 +9,10 @@ void setup() {
   Serial1.begin(9600);
   Serial.begin(9600);
 
-
+  while (digitalRead(3) != LOW) {
+    cout << analogRead(1) << " ";
+  }
+            
 
   #ifdef TEST_INTERFACE_H
     testInterface();
