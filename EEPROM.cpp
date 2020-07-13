107e8cd9bd71afe65171b32d715079de9a30220a
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-07-13 21:50:15 +0100

    EEPROM now fetched usin references

--- EEPROM.cpp
+++ EEPROM.cpp
@@ -5,19 +5,54 @@
 
 // Constructors
 EEPROM_manager::EEPROM_manager() {
-    tableSpan = gsl::make_span(table); // Creates the span referencing the EEPROM table
+    //tableRef = table;
+    //tableSpan = gsl::make_span(table); // Creates the span referencing the EEPROM table
 }
 
-EEPROM_manager::block::block(int _size) { // creates a memory block. It is reccommended to set 'size' larger than needed for future proofing.
-    size = _size;
+EEPROM_manager::block::block(vector<int> _dimensions) { // creates a memory block of dimensions {x,y,z} (max 3D) e.g a 1D array of 10: {10}
+    auto dimensions = _dimensions;
+    noOfDimensions = dimensions.size();
+    size = 1;
+    for (auto i : dimensions) {
+        size = size * i; // finds the total size of the block as the product of its dimensions
+    }
     if (! blockAddresses.empty()) {
         start = blockAddresses.back()[1] + 1; // new memory block starts after the last listed block
     } else {
         start = 0;
     }
     end = start + size - 1;
+    if (end >= EEPROMsize) {
+        Serial.println("ERROR: Block exceeds EEPROM size")
+        break;
+    }
     blockAddresses[blockAddresses.size()][0] = start; // adds the new block to the address list
     blockAddresses[blockAddresses.size()][1] = end;
+    /*
+    switch (noOfDimensions) {
+        case 1:
+
+        case 2:
+
+        case 3:
+            for (int i = 0; i <= dimensions[0]; i++) {
+                for (int j=0; j <= dimensions[1]; j++) {
+                    data3D[i][j]= tableSpan.subspan()
+                }   
+            }
+    }
+    */
+}
+
+EEPROM_manager::block::blockIndex(vector<int> index[]) { // returns the index within block given the array dimensions
+    switch (noOfDimensions) {
+        case 1: 
+            return index[0];
+        case 2:
+            return index[0]*dimensions[1] + index[1] - 1
+        case 3:
+            return index[0]*dimensions[1]*dimensions[2] + index[1]*dimensions[2] + index[0] - 1
+    }
 }
 
 // Method definitions:
@@ -39,9 +74,9 @@ void EEPROM_manager::save() {  // saves all changed values from memory into EEPR
     }
 }
 
-int EEPROM_manager::fetch(int address) { // fetches the value from the version stored in memory
+int& EEPROM_manager::fetch(int address) { // fetches a reference (allowing direct modification) to the value from the version stored in memory
     if (address < size && address >= 0) { // these are the range of values possible
-        return(EEPROM.read(address));
+        return(table[address]);
     }
 	else {
 		Serial.println("ERROR: Invalid EEPROM address");
@@ -66,12 +101,15 @@ void EEPROM_manager::write(int address, int data, bool save) { // saves to the v
     }
 }
 
-int EEPROM_manager::block::blockFetch(int address) { // retrieves the value from the EEPROM table, indexed within the memory block of the block class instance (e.g. a block containing 61 key positions is indexed 0-60)
-    return fetch(address + start);
+int& EEPROM_manager::block::blockFetch(vector<int> index) { // fetches a reference to the value in the EEPROM table (thus can be modified directly), indexed within the memory block of the block class instance. E.g. for a 1D block: {5} or 5; or for a 2D block: {3,5}
+    return fetch(blockIndex(index) + start);
+}
+int& EEPROM_manager::block::blockFetch(int index) { // fetches a reference to the value in the EEPROM table (thus can be modified directly), indexed within the memory block of the block class instance. E.g. for a 1D block: {5} or 5; for a 2D block: {3,5}
+    return fetch(index + start);
 }
     
-void EEPROM_manager::block::blockWrite(int address, int data, bool save) { // writes the to address within the block
-    write(address + start, data, save);
+void EEPROM_manager::block::blockWrite(vector<int> index, int data, bool save) { // writes the to address within the block
+    write(blockIndex(index) + start, data, save);
 }
 
 gsl::span<int> EEPROM_manager::block::fetchSpan(int startAddress, int endAddress) { // returns a span (similar to vector) for the given range which acts as a reference to the relevant part of the EEPROM. Set endAddress to -1 to fetch the entire block
--- EEPROM.h
+++ EEPROM.h
@@ -10,10 +10,11 @@
 
 
 class EEPROM_manager {
-    int size = 1024; // The size of the EEPROM on the microcontroller in bytes. 1024 on Teensy 4.0 but 4284 on Teensy 4.1
+    int EEPROMsize = 1024; // The size of the EEPROM on the microcontroller in bytes. 1024 on Teensy 4.0 but 4284 on Teensy 4.1
     vector<vector<int>> blockAddresses {} // stores the start and end of the addresses of EEPROM 'blocks'
     vector<int> table; // Stores a live version of the EEPROM
-    gsl::span<int> tableSpan; // stores reference to EEPROM table
+    //vector<reference_wrapper<int>> tableRef;
+    //gsl::span<int> tableSpan; // stores a reference to EEPROM table
 public:
     EEPROM_manager();
     void load(); // loads EEPROM into memory
@@ -22,12 +23,24 @@ public:
     void write(int address, int data, bool save=false); // saves to the version stored in memory
 
     class block {
-
-    public:
+        vector<int> dimensions;
+        int noOfDimensions;
         int size, start, end;
-        block(int size); // creates an EEPROM 'block'. It is reccommended to set 'size' larger than needed for future proofing.
+        
+        int blockIndex(vector<int> index); // returns the index within block given the array dimensions
+        /*
+        gsl::span<int> data1D;
+        gsl::span<gsl::span<int>> data2D;
+        gsl::span<gsl::span<gsl::span<int>>> data3D;
+        vector<reference_wrapper<int>> data1D;
+        vector<reference_wrapper<int>> data2D;
+        vector<reference_wrapper<int>> data3D;
+        */
+        
+    public:
+        block(int size, vector<int> _dimensions); // creates an EEPROM 'block'. It is reccommended to set 'size' larger than needed for future proofing.
         int blockFetch(int address); // returns single value from 'local' address within block
-        int& blockRefFetch(int address); // returns a reference to an item in the block
+        //int& blockRefFetch(int address); // returns a reference to an item in the block
         void blockWrite(int address, int data, bool save=false);
         gsl::span<int> fetchSpan(int startAddress, int endAddress = -1); // returns a vector referencing part of or all of the block
     }
