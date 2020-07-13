412cdf962a35cbba52d70e7d0ddb8f34ba2eed57
PixelMutation <34405284+PixelMutation@users.noreply.github.com>
2020-07-13 21:54:55 +0100

    Revert "Teensy port"

--- utility.h
+++ utility.h
@@ -5,12 +5,10 @@
 
 #include "modules.h"
 
-
 void printVector(vector<int> vector, bool displayZero = true);
 void printVector(deque<double> vector, bool displayZero = true);
 void printKeyStates(string option = "full");
 void printStopStates(string option = "full");
-
 
 
 #endif
