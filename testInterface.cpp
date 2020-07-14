782f52b29b15bb29dc34fb1399811eefcb903903
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-07-14 14:07:03 +0100

    Replaced cout with Serial.print()

--- testInterface.cpp
+++ testInterface.cpp
@@ -7,23 +7,23 @@ void testInterface() {
     Serial.println("\nWelcome to the Player Organ Test Interface!");
     while (choice != -1) {
         
-        cout << "\nChoose testing options: 1. Simulate key / stop presses 2. Test key scheduling 3. Simulate MIDI input 4. Test hall sensors or -1. to end: ";
+        Serial.println("Choose testing options: 1. Simulate key / stop presses 2. Test key scheduling 3. Simulate MIDI input 4. Test hall sensors or -1. to end: ");
         cin >> choice;
         switch (choice) {
         case 1:
             choice = 0;
             while (choice != -1) {
 
-                cout << "\n1. Keys or 2. Stops? or -1 to exit: ";
+                Serial.println("1. Keys or 2. Stops? or -1 to exit: ");
                 cin >> choice;
 
                 if (choice == 1) {
-                    cout << "\nChoose item number (1-61) or -1 to stop: ";
+                    Serial.println("Choose item number (1-61) or -1 to stop: ");
                     cin >> number;
                     while (number != -1 and number > 0) {
-                        cout << "Choose state: 0/1: ";
+                        Serial.println("Choose state: 0/1: ");
                         cin >> state;
-                        cout << "\n";
+                        Serial.println("");
                         keypressHandler(number, state);
                         printKeyStates("header");
                         printKeyStates();
@@ -33,26 +33,26 @@ void testInterface() {
                             delay(200);
                             //this_thread::sleep_for(chrono::milliseconds(200));
                         }
-                        cout << "\nkeys pressed overall  : ";    Keys.getStatesVector("all", true, true);
+                        Serial.println("keys pressed overall  : ");    Keys.getStatesVector("all", true, true);
 
-                        cout << "\n\nChoose item number (1-61) or -1 to stop.";
+                        Serial.println("Choose item number (1-61) or -1 to stop: ");
                         cin >> number;
                     }
 
                 }
                 else if (choice == 2) {
-                    cout << "\nChoose item number (1-11) or -1 to stop: ";
+                    Serial.println("Choose item number (1-61) or -1 to stop: ");
                     cin >> number;
                     while (number != -1 and number > 0) {
-                        cout << "\nChoose state: 0/1: ";
+                        Serial.println("Choose state: 0/1: ");
                         cin >> state;
-                        cout << "\n";
+                        Serial.println("");
                         Stops.setSensorState(number, state);
 
                         printStopStates("header");
                         printKeyStates();
 
-                        cout << "\n\nChoose item number (1-11) or -1 to stop: ";
+                        Serial.print("Choose item number (1-61) or -1 to stop: ");
                         cin >> number;
                     }
 
@@ -60,7 +60,7 @@ void testInterface() {
                 }
 
             }
-            cout << "\nChoose testing options: 1. Simulate key / stop presses 2. Test key scheduling 3. Simulate MIDI input or -1. to end: ";
+            Serial.println("Choose testing options: 1. Simulate key / stop presses 2. Test key scheduling 3. Simulate MIDI input 4. Test hall sensors or -1. to end: ");
             cin >> choice;
             break;
         case 2:
@@ -68,11 +68,11 @@ void testInterface() {
             int key;
             while (choice != -1) {
                 
-                cout << "\ntype key number: ";
+                Serial.println("type key number: ");
                 cin >> key;
                 keypressHandler(key, 1);
                 printKeyStates("overall");
-                cout << "\n";
+                Serial.println("");
 
                 while (! Keys.schedule.empty()) {
                     checkSchedule();
@@ -82,7 +82,7 @@ void testInterface() {
                 printKeyStates("overall");
 
             }
-            cout << "\nChoose testing options: 1. Simulate key / stop presses 2. Test key scheduling 3. Simulate MIDI input or -1. to end: ";
+            Serial.println("Choose testing options: 1. Simulate key / stop presses 2. Test key scheduling 3. Simulate MIDI input 4. Test hall sensors or -1. to end: ");
             cin >> choice;
             break;
 
@@ -92,18 +92,18 @@ void testInterface() {
             int status, data1, data2;
 
             while (choice != -1) {
-                cout << "\n1. Raw MIDI data 2. Instrument change 3. Key on / off or -1 to exit: "; cin >> choice;
+                Serial.println("1. Raw MIDI data 2. Instrument change 3. Key on / off or -1 to exit: "); cin >> choice;
                 
                 switch (choice) {
                 case 1:
 
-                    cout << "\nStatus byte (1st nibble = command type, 2nd nibble = channel no.) e.g. 0x81: "; 
+                    Serial.println("Status byte (1st nibble = command type, 2nd nibble = channel no.) e.g. 0x81: "); 
                     cin >> hex >> status;
                     cout << status;
-                    cout << "\nData1 byte (e.g. instrument number, note number) e.g. 48: "; 
+                    Serial.println("Data1 byte (e.g. instrument number, note number) e.g. 48: "); 
                     cin  >> dec >> data1;
                     cout << data1;
-                    cout << "\nData2 byte (e.g. velocity, other data) e.g. 100: "; 
+                    Serial.println("Data2 byte (e.g. velocity, other data) e.g. 100: "); 
                     cin >> dec >> data2;
                     cout << data2 << "\n\n";
 
@@ -121,12 +121,12 @@ void testInterface() {
                 }
                 
             }
-            cout << "Choose testing options: 1. Simulate key / stop presses 2. Test key scheduling 3. Simulate MIDI input or -1. to end: ";
+            Serial.println("Choose testing options: 1. Simulate key / stop presses 2. Test key scheduling 3. Simulate MIDI input 4. Test hall sensors or -1. to end: ");
             cin >> choice;
             break;
         case 4:
             while (true) {
-                cout << testMux.muxRead(8,true,false) << "\n ";
+                Serial.println("testMux.muxRead(8,true,false)");
             }
             break;
         case -1:
--- utility.cpp
+++ utility.cpp
@@ -1,23 +1,27 @@
 ﻿#include "utility.h"
 
+
 void printVector(vector<int> vector, bool displayZero) {
 	for (int element : vector) {
 
 		if (element != 0) {
-			cout << " " << element;
+			Serial.print(" ");
+			Serial.print(element);
+
 		}
 		else {
 			if (displayZero) {
-				cout << " " << element;
+				Serial.print(" ");
+				Serial.print(element);
 			}
 			else {
-				cout << "  ";
+				Serial.print("  ");
 			}
 		}
 		
 
 
-		cout << " ";
+		Serial.print(" ");
 	}
 
 }
@@ -26,56 +30,58 @@ void printVector(deque<double> vector, bool displayZero) {
 	for (double element : vector) {
 
 		if (element != 0) {
-			cout << " " << element;
+			Serial.print(" ");
+			Serial.print(element);
 		}
 		else {
 			if (displayZero) {
-				cout << " " << element;
+				Serial.print(" ");
+				Serial.print(element);
 			}
 			else {
-				cout << "  ";
+				Serial.print("  ");
 			}
 		}
 
 
 
-		cout << " ";
+		Serial.print(" ")
 	}
 
 }
 
 void printKeyStates(string option) {
 	if (option == "full") {
-		cout << "\nkeys pressed by user  : ";    Keys.getStatesVector("user", true, false);
-		cout << "\nkeys pressed overall  : ";    Keys.getStatesVector("all", true, false);
-		cout << "\nkeys pressed by system: ";    Keys.getStatesVector("system", true, false);
-		cout << "\n";
+		
+		Serial.print("\nkeys pressed by user  : ");    Keys.getStatesVector("user", true, false);
+		Serial.print("\nkeys pressed overall  : ");    Keys.getStatesVector("all", true, false);
+		Serial.print("\nkeys pressed by system: ");    Keys.getStatesVector("system", true, false);
 	} else if (option == "buffer") {
-		cout << "\nkey requests  buffer  : ";    Keys.getStatesVector("buffer", true, false);
+		Serial.print("\nkey requests  buffer  : ");    Keys.getStatesVector("buffer", true, false);
 	} else if (option =="overall") {
-		cout << "\nkeys pressed overall  : ";    Keys.getStatesVector("all", true, false);
+		Serial.print("\nkeys pressed overall  : ");    Keys.getStatesVector("all", true, false);
 	} else if (option == "user") {
-		cout << "\nkeys pressed by user  : ";    Keys.getStatesVector("user", true, false);
+		Serial.print("\nkeys pressed by user  : ");    Keys.getStatesVector("user", true, false);
 	} else if (option == "system") {
-		cout << "\nkeys pressed by system: ";    Keys.getStatesVector("system", true, false);
+		Serial.print("\nkeys pressed by system: ");    Keys.getStatesVector("system", true, false);
 	} else if (option == "header") {
-		cout << "\n                        01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 ";
+		Serial.print("\n                        01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 ");
 	} 
 }
 
 void printStopStates(string option) {
 	if (option == "full") {
-		cout << "\n stops user state   : "; Stops.getStatesVector("user", true, true);
-		cout << "\n stops overall state: "; Stops.getStatesVector("all", true, true);
-		cout << "\n stops system state : "; Stops.getStatesVector("system", true, true);
+		Serial.print("\n stops user state   : "); Stops.getStatesVector("user", true, true);
+		Serial.print("\n stops overall state: "); Stops.getStatesVector("all", true, true);
+		Serial.print("\n stops system state : "); Stops.getStatesVector("system", true, true);
 	} else if (option =="overall") {
-		cout << "\n stops overall state: "; Stops.getStatesVector("all", true, true);
+		Serial.print("\n stops overall state: "); Stops.getStatesVector("all", true, true);
 	} else if (option == "user") {
-		cout << "\n stops user state   : "; Stops.getStatesVector("user", true, true);
+		Serial.print("\n stops user state   : "); Stops.getStatesVector("user", true, true);
 	} else if (option == "system") {
-		cout << "\n stops system state : "; Stops.getStatesVector("system", true, true);
+		Serial.print("\n stops system state : "); Stops.getStatesVector("system", true, true);
 	} else if (option == "header") {
-		cout << "\n                       01 02 03 04 05 06 07 08 09 10 11";
+		Serial.print("\n                       01 02 03 04 05 06 07 08 09 10 11");
 	}
 }
 
