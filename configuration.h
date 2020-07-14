67736feb82fbe823dd7cd1f53a06f762066c001a
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-07-14 14:07:42 +0100

    Added configuration header

---
+++ configuration.h
@@ -0,0 +1,25 @@
+// The main config for the code - replace these values to match your setup.
+
+#include "modules.h"
+
+// BASICS
+
+// Microcontroller Settings:
+#define EEPROM_SIZE  1024 // amount in Teensy 4.0, for 4.1 it is
+
+
+// Keyboard / Manuals Settings:
+#define NUM_MANUALS 1
+#define KEYS_PER_MANUAL  61 
+#define POLYPHONY  10      //number of keys the system can hold down at once
+#define FIRST_NOTE "A" // the first key of the manual
+
+// Stops settings:
+#define NUM_STOPS  11
+
+// Bass pedals settings:
+#define NUM_PEDALS 0
+#define FIRST_BASS_NOTE "A" // the first key of the bass pedals
+
+
+// Control panel settings:
