06e7523e8542da5a4913c66c8920be7085ccc8a9
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-08-12 18:06:18 +0100

    Tidied up main file, added module call hooks

--- playerReedOrgan.cpp
+++ playerReedOrgan.cpp
@@ -2,83 +2,49 @@
 
 #include "modules.h"
 #include "Arduino.h"
+#include "Plotter.h"
 
+int a, b,c;
+
+Plotter p;
 int led = 13;
 
 void setup() {
+  
+
+
+
   // put your setup code here, to run once:
-  
-  
+
+  p.Begin();
+  p.AddTimeGraph("Hall effect sensors",1000, "LB", a, "hall", b, "UB", c);
 
   Serial1.begin(9600);
   Serial.begin(9600);
   
 
   pinMode(led, OUTPUT);
-  Serial.println("1 2 3 4 5 6 7 8 Hall Reed Pot Hall2\n");
-  int Delay = 0;
+  //Serial.println("1 2 3 4 5 6 7 8 Hall Reed Pot Hall2\n");
+  
+
   while (/*testMux.muxRead(9,false,true) != LOW*/true) {
+
     digitalWrite(led, HIGH);
-    Serial.print("\n");
-    /*
-    for (int i = 0; i < 15; i++) {
+    //Serial.print("\n");
 
-      //Delay = testMux.muxRead(10,true,true);
+    a= 150;
+    b= testMux.muxRead(8,true,true);
+    c= 210;
 
-      Serial.print(testMux.muxRead(i,true,true,Delay));
-      Serial.print(" ");
-      
-    }
-    */
-    Serial.print(testMux.muxRead(8,true,true,Delay));
-    Serial.print(" ");
-    Serial.print(testMux.muxRead(11,true,true,Delay));
-    Serial.print(" ");
-    Serial.print(255);
-    Serial.print(" ");
-    Serial.print(0);
-    digitalWrite(led, LOW);
+    p.Plot();
 
+    delayMicroseconds(1000); 
   }
-  
-  /*
-  int x =0;
-  while (x < 10) {
-    x++;
-    Serial.println("\n TEST");
-    delay(1000);
-    if (testMux.muxRead(0,false,true) == HIGH) {
-      Serial.println("\n x");
-    }
-    Serial.println("\n y");
-  }
-  */
-  
-  /*
-  #ifdef SCALES_H
-    //cout << "y";    
-    if (testMux.muxRead(0,false,true) == HIGH) {
-      scaleManager.scale = "harmonic minor";
-    } else {
-      scaleManager.scale = "major";
-    }
-    if (testMux.muxRead(1,false,true) == HIGH) {
-      scaleManager.chordType = "seventh";
-    } else {
-      scaleManager.chordType = "triad";
-    }
-    if (testMux.muxRead(2,false,true) == HIGH) {
-      scaleManager.delay = 0;
-    } else {
-      scaleManager.delay = 20;
-    }
-    scaleManager.updatePatterns();
-  #endif
-  */
 
-  #ifdef TEST_INTERFACE_H
-    testInterface();
-  #endif
+  // Calls all module functions set to run on startup
+  for (auto module : moduleCall.OnStart) {
+    module->onStart();
+  }       
 }
 
 void loop() {
@@ -86,8 +52,12 @@ void loop() {
   // put your main code here, to run repeatedly:
   checkSchedule();
   Controls.checkPins();
+
+  // Calls all module functions set to run each loop
+  for (auto module : moduleCall.OnLoop) {
+    module->onLoop();
+  }
+
+  
 }
 
-void serialEvent() {
-  MIDI.MIDIrecieve();
-}
