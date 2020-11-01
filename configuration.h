d414c6b4e3966e83d881e2b8de1d535ec652db73
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-11-01 18:35:53 +0000

    Added more config options

--- configuration.h
+++ configuration.h
@@ -2,15 +2,14 @@
 
 // BASICS
 
-// Microcontroller Settings:
+// MICROCONTROLLER SETTINGS:
 #define EEPROM_SIZE  1024   // amount in Teensy 4.0, for 4.1 it is larger
 #define NUM_I2C_PORTS 2     // there are 2 on Teensy 4.0 and 3 and Teensy 4.1
 
-// Pins
 
 
 
-// Keyboard / Manuals Settings:
+// KEYBOARD/MANUAL SETTINGS:
 #define NUM_MANUALS 1 
 #define KEYS_PER_MANUAL  61  // the product of NUM_MANUALS and KEYS_PER_MANUAL must not exceed 256
 
@@ -21,17 +20,36 @@
 #define COOLDOWN_TRIGGER 60 // any keypresses longer than this require a cooldown before being used again
 #define COOLDOWN 0      // the cooldown duration
 
-// Sensors settings:
+// SENSORS SETTINGS:
 #define INTERNAL_ADC false // whether the internal ADC is used 
+#define ANALOG true // whether the sensors are analog (should be)
+#define ADC_AVERAGING 16 // the averaging level of the ADCs
+#define MICROSECOND_DELAY 0 // the delay in microseconds before taking a measurement to allow stabilisation
 
-// Stops settings:
+// PINS:
+#define KEY_SIG A9 // has to be supported by Continuous read
+#define STOP_SIG A8
+#define PED_SIG A7
+#define CTRL_SIG A6
+
+#define KEY_PULLUP false // whether the sensors are pullup or not (can be a mix for CTRL)
+#define STOP_PULLUP false
+#define PED_PULLUP false
+
+#define CONFIRM_PIN 14 // connected to a button as a method of confirming something (such as proceeding to next calibration step)
+#define CONFIRM_PIN_MODE INPUT // either INPUT or INPUT_PULLUP depending on the button's wiring
+
+#define MUX_SELECT_PINS {2,3,4,5} // the pins used to select an individual I/O pin on a mux
+#define MUX_ENABLE_PINS {9,10,11,12} // the pins used to select which mux to enable via a 4-16 inverting demux
+
+// STOPS SETTINGS:
 #define NUM_STOPS  11   // total number of stops (<=128)
 #define DIVISIONS {11}  // defines how the stops are split into different divisions
 
 
-// Bass pedals settings: (currently not actuated)
+// BASS PEDALS SETTINGS: (currently not actuated)
 #define NUM_BASS_PEDALS 0
 #define FIRST_BASS_NOTE "A" // the first key of the bass pedals
 
 
-// Control panel settings:
+// CONTROL PANEL SETTINGS:
