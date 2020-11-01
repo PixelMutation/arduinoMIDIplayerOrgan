d1daf7d05bb1ec13ea6d7a81549ef744b905fa5f
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-11-01 00:25:31 +0000

    added I2C management and port expander support

---
+++ I2Cmanager.cpp
@@ -0,0 +1,69 @@
+#include "I2Cmanager.h"
+
+I2C_manager::I2C_manager (int _port, int _numDevices, int _baseAddress) {
+    port = _port;
+    numDevices = _numDevices;
+    baseAddress = _baseAddress;
+    switch (port) {
+    case 0:
+        Wire.begin();
+        wire = &Wire;
+        break;
+    case 1:
+        Wire1.begin();
+        wire = &Wire1;
+        break;
+    case 2:
+        Wire2.begin();
+        wire = &Wire2;
+        break;
+    }
+    for (int i=0; i< numDevices; i++) {
+        devices.push_back(baseAddress+i);
+    }
+}
+
+void I2C_manager::write(vector<int> bytes, int device) {
+    wire->beginTransmission(devices[device]);
+    for (int byte : bytes) {
+        wire->write(byte);
+    }
+    wire->endTransmission();
+}
+int I2C_manager::read(int device) {
+    if (wire->available() > 0) {
+        return wire->read();
+    }
+}
+
+portExpander::portExpander(int _port, int _numDevices) {
+    port = _port;
+    numDevices = _numDevices;
+    I2C_manager expanders(port,numDevices,baseAddress);
+    for (int i = 0; i < numDevices; i++) { 
+        expanders.write({0x00,0x00},i); // A register set to output
+        expanders.write({0x01,0x00},i); // B register set to output
+        regA.push_back(0); // sets stored pin states to 0
+        regB.push_back(0);
+    }
+}
+
+void portExpander::write(int data, int device) {
+    expanders.write({data},device);
+}
+
+void portExpander::writeToPin(int pin, int state) {
+    int device = pin/16; // finds device number
+    int devicePin = (pin-device*16); // finds the pin on the specific expander
+    int reg;
+    if (devicePin < 8) { // if in register A
+        reg = 0x12;        //set register address
+        regA[device]  ^= (-state ^ regA[device]) & (1UL << devicePin); // updates byte containing states using individual bit changing method from https://stackoverflow.com/questions/47981/how-do-you-set-clear-and-toggle-a-single-bit
+        expanders.write({reg},regA[device]) // writes byte to register
+    } else {
+        reg = 0x13;
+        regB[device] ^= (-state ^ regA[device]) & (1UL << devicePin-8);
+        expanders.write({reg},regA[device])
+    }
+    
+}
---
+++ I2Cmanager.h
@@ -0,0 +1,38 @@
+#ifndef I2C_MANAGER
+#define I2C_MANAGER
+
+#include "global_includes.h"
+#include "Wire.h"
+
+class I2C_manager {
+    TwoWire * wire;
+    int port;
+    int numDevices;
+    vector<int> devices;
+    int baseAddress;
+
+
+public:
+    I2C_manager(int _port, int _numDevices, int _baseAddress);
+    void write(vector<int> bytes, int device);
+    int read(int device);
+
+};
+
+class portExpander { // set up for the MCP23017
+    int numDevices; // the number of port expanders
+    int port;
+    int baseAddress = 0x20;
+    I2C_manager expanders;
+    vector<int> regA;
+    vector<int> regB;
+
+public:
+    portExpander(int _port, int _numDevices);
+    void write(int data, int device );
+    void writeToPin(int pin, int state);
+    
+
+};
+
+#endif
