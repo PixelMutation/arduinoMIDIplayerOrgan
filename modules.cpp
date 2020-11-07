204a6c9323a658b0e56a2e0c2662ac8c1f37f4b5
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-11-07 17:24:28 +0000

    updated to include new parameter

--- modules.cpp
+++ modules.cpp
@@ -3,8 +3,8 @@
 
 void instantiateModules() {
     for (int i=0; i < NUM_MANUALS; i++) {
-        manuals.push_back(new stateManager("keys", KEYS_PER_MANUAL, POLYPHONY));
+        manuals.push_back(new stateManager("keys", KEYS_PER_MANUAL, POLYPHONY,0));
     }
-    stateManager Stops("stops", NUM_STOPS, NUM_STOPS);
+    stateManager Stops("stops", NUM_STOPS, NUM_STOPS,0);
 
 }
