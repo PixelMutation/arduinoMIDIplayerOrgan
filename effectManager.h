7ee7ddc3c378c75aa7c591a9b62ca23d0f971f46
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-11-17 22:24:48 +0000

    changed output format

--- effectManager.h
+++ effectManager.h
@@ -5,8 +5,14 @@
 
 
 class effectTemplate {
+private:
+    std::vector<std::tuple<int,int,int,unsigned long>> output;
+public:
+    
+    friend class effectManager; // the effectManager is a friend so can access the private variables
     bool skip; // whether the result of this one should be skipped
-    vector<int> applyEffect(vector<int> original, vector<int>);
+    std::vector<int> applyEffect(std::vector<int> original, std::vector<std::tuple<int,int,int,unsigned long>> );
+
 };
 
 
@@ -16,15 +22,21 @@ class effectManager {
     
     
 public:
+    effectManager();
+
+
     void addEffect(effectTemplate* effect); // adds new effect to master list
     int numActiveFx();
 
     void pushBackActive(effectTemplate* effect); // adds effect to end of list
     void pushFrontActive(effectTemplate* effect); // adds effect to start of list
     void insertActive(effectTemplate* effect,int pos);    //adds effect at a particular point (the end if invalid index given)
+    void removeActive(int index);
+    void popBackActive();
+    void popFrontActive();
 };
 
-effectManager Effects
+effectManager Effects;
 
 
 
