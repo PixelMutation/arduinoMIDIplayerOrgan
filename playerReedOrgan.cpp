6394ab5f0b5090299011c1e191d73daed4a7a04c
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-11-17 22:29:37 +0000

    I2C test script added

--- playerReedOrgan.cpp
+++ playerReedOrgan.cpp
@@ -12,55 +12,27 @@
 
 
 
-void setup() {
+void setup() { 
 
-  //instantiateModules(); // creates instances of all the modules and plugins
+  console.println("reached setup");
+
+  pinMode(13,OUTPUT);
 
   
-  // put your setup code here, to run once:
-  /*
-  p.Begin();
-  p.AddTimeGraph("Hall effect sensors",1000, "LB", a, "hall", b, "UB", c);
-
-  Serial1.begin(9600);
-  Serial.begin(9600);
   
-
-  pinMode(led, OUTPUT);
-  //Serial.println("1 2 3 4 5 6 7 8 Hall Reed Pot Hall2\n");
   
-
-  while (testMux.muxRead(9,false,true) != LOW) {
-
-    digitalWrite(led, HIGH);
-    //Serial.print("\n");
-
-    a= 150;
-    b= testMux.muxRead(8,true,true);
-    c= 210;
-
-    p.Plot();
-
-    delayMicroseconds(1000); 
-  }
-  */
-
-
-
-  while (true) {
-    KeyActuators.setState(0,0,1);
-    delay(100);
-    KeyActuators.setState(0,0,1);
-  }
-  
-
-
   // Calls all module functions set to run on startup
   hook.start();   
 }
 
 void loop() {
   
+  digitalWrite(13,HIGH);
+  KeyActuators.setState(0,0,0);
+  delay(analogRead(22));
+  digitalWrite(13,LOW);
+  KeyActuators.setState(0,0,1);
+  delay(analogRead(21));
   // Calls all module functions set to run each loop
   hook.loop();
 
