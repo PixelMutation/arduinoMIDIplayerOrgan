6a59a7fb4c86563358d1e29be03e26b8851b9312
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-11-22 17:57:30 +0000

    uses classes for hooks & reduced duplicate code

--- moduleManager.cpp
+++ moduleManager.cpp
@@ -1,143 +1,245 @@
 #include "moduleManager.h"
 
-void moduleTemplate::onStart() {
+/* -------------------------------------------------------------------------- */
+/*                     VIRTUAL HOOK CALL TEMPLATE METHODS                     */
+/* -------------------------------------------------------------------------- */
 
-}
-void moduleTemplate::onLoop() {}
-
-void moduleTemplate::onKeyMove         (int key, int newPos, int oldPos) {}
-void moduleTemplate::onUserKeyToggle   (int manual, int key, int velocity, bool state, int pos) {}
-void moduleTemplate::onSystemKeyToggle (int manual, int key, int velocity, bool state) {}
-
-void moduleTemplate::onUserStopToggle  (int stop, bool state) {}
-void moduleTemplate::onSystemStopToggle(int stop , bool state) {}
-
-void moduleTemplate::onPedalToggle     (int pedal, bool state) {}
-
-void moduleTemplate::onControlChange   (int panel, int number, int state) {}
-
-void moduleTemplate::onMidiKey         (int manual, int key  , int velocity) {}
-void moduleTemplate::onMidiCC          (int data1 , int data2) {}
-void moduleTemplate::onMidiCCmod       (int data) {}
-void moduleTemplate::onMidiCCsustain   (int data) {}
-void moduleTemplate::onMidiCCchorus    (int data) {}
-void moduleTemplate::onMidiCClegato    (int data) {}
-void moduleTemplate::onMidiCCvolume    (int data) {}
-void moduleTemplate::onMidiInstrument  (int instrument) {}
-
-void moduleTemplate::onSerialMessage   (std::string module, std::string setting, int data) {}
+void moduleTemplate::onStart            (PARAM_onStart           ) {}
+void moduleTemplate::onLoop             (PARAM_onLoop            ) {}
+void moduleTemplate::onKeyMove          (PARAM_onKeyMove         ) {}
+void moduleTemplate::onUserKeyToggle    (PARAM_onUserKeyToggle   ) {}
+void moduleTemplate::onSystemKeyToggle  (PARAM_onSystemKeyToggle ) {}
+void moduleTemplate::onUserStopToggle   (PARAM_onUserStopToggle  ) {}
+void moduleTemplate::onSystemStopToggle (PARAM_onSystemStopToggle) {}
+void moduleTemplate::onPedalToggle      (PARAM_onPedalToggle     ) {}
+void moduleTemplate::onControlChange    (PARAM_onControlChange   ) {}
+void moduleTemplate::onMidiKey          (PARAM_onMidiKey         ) {}
+void moduleTemplate::onMidiCC           (PARAM_onMidiCC          ) {}
+void moduleTemplate::onMidiCCmod        (PARAM_onMidiCCmod       ) {}
+void moduleTemplate::onMidiCCsustain    (PARAM_onMidiCCsustain   ) {}
+void moduleTemplate::onMidiCCchorus     (PARAM_onMidiCCchorus    ) {}
+void moduleTemplate::onMidiCClegato     (PARAM_onMidiCClegato    ) {}
+void moduleTemplate::onMidiCCvolume     (PARAM_onMidiCCvolume    ) {}
+void moduleTemplate::onMidiInstrument   (PARAM_onMidiInstrument  ) {}
+void moduleTemplate::onSerialMessage    (PARAM_onSerialMessage   ) {}
 
 
-void Hooks::start() {
-    for (auto module : OnStart) {
-		module->onStart();
-	}
-}
-void Hooks::loop() {
-    for (auto module : OnLoop) {
-        module->onLoop();
-    }
+
+/* -------------------------------------------------------------------------- */
+/*                            HOOK TEMPLATE METHODS                           */
+/* -------------------------------------------------------------------------- */
+
+Hooks::hookTemplate::hookTemplate() {
+    console.println("hook template setup");
+    hookNum = hooks.numHooks;
+    hooks.numHooks+=1;
+    hooks.hookModules.resize(hooks.numHooks);
 }
 
-void Hooks::keyMove(int key, int newPos, int oldPos) {
-    for (auto module : OnKeyMove) {
-        module->onKeyMove(key,newPos,oldPos);
-    }
+int Hooks::hookTemplate::add(moduleTemplate* module) { // appends module to hooks list, returning position for use in .remove
+    hooks.setup();
+    console.println("add hook");
+    int pos;
+    if (hooks.hookModules.empty()) {console.println("empty");}
+    if (!! hooks.hookModules[hookNum].empty()) {
+         pos = (int)hooks.hookModules[hookNum].size();
+    }  else {pos=0;}
+    console.println(pos);
+    console.println(hookNum);
+
+    hooks.hookModules[hookNum].push_back(module);
+    console.println("added");
+    return pos;
 }
-void Hooks::userKeyToggle(int manual, int key, int velocity, bool state, int pos) {
-    for (auto module : OnUserKeyToggle) {
-        module->onUserKeyToggle(manual,key,velocity,state,pos);
-    }
+bool Hooks::hookTemplate::activate(moduleTemplate* module, int pos) { // reinstates the module at a position
+    if (pos >= 0 && pos < (int)hooks.hookModules[hookNum].size()) {
+        hooks.hookModules[hookNum][pos] = module;
+        return true;
+    } else {return false;}
 }
-void Hooks::systemKeyToggle(int manual, int key, int velocity, bool state) {
-    for (auto module : OnSystemKeyToggle) {
-        module->onSystemKeyToggle(manual,key,velocity,state);
-    }
+bool Hooks::hookTemplate::deactivate(int pos) { // sets the value here to NULL so nothing happens. Reinstated with .activate
+    if (pos >= 0 && pos < (int)hooks.hookModules[hookNum].size()) {
+        hooks.hookModules[hookNum][pos] = NULL; // replaces it with NULL to preserve positions of other elements
+        return true;
+    } else {return false;}
 }
 
-void Hooks::userStopToggle(int stop, bool state) {
-    for (auto module : OnUserStopToggle) {
-        module->onUserStopToggle(stop,state);
-    }
-}
-void Hooks::systemStopToggle(int stop, bool state) {
-    for (auto module : OnSystemStopToggle) {
-        module->onSystemStopToggle(stop,state);
-    }
-}
+/* -------------------------------------------------------------------------- */
+/*                              HOOK CALL METHODS                             */
+/* -------------------------------------------------------------------------- */
 
-void Hooks::pedalToggle(int pedal, bool state) {
-    for (auto module : OnPedalToggle) {
-        module->onPedalToggle(pedal,state);
-    }
-}
+void Hooks::onStart::call
+    (PARAM_onStart) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onStart
+    (ARG_onStart);
+    }}
+void Hooks::onLoop::call
+    (PARAM_onLoop) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onLoop
+    (ARG_onLoop);
+    }}
+void Hooks::onKeyMove::call
+    (PARAM_onKeyMove) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onKeyMove
+    (ARG_onKeyMove);
+    }}
+void Hooks::onUserKeyToggle::call
+    (PARAM_onUserKeyToggle) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onUserKeyToggle
+    (ARG_onUserKeyToggle);
+    }}
+void Hooks::onSystemKeyToggle::call
+    (PARAM_onSystemKeyToggle) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onSystemKeyToggle
+    (ARG_onSystemKeyToggle);
+    }}
+void Hooks::onUserStopToggle::call
+    (PARAM_onUserStopToggle) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onUserStopToggle
+    (ARG_onUserStopToggle);
+    }}
+void Hooks::onSystemStopToggle::call
+    (PARAM_onSystemStopToggle) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onSystemStopToggle
+    (ARG_onSystemStopToggle);
+    }}
+    
+void Hooks::onPedalToggle::call
+    (PARAM_onPedalToggle) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onPedalToggle
+    (ARG_onPedalToggle);
+    }}
+void Hooks::onControlChange::call
+    (PARAM_onControlChange) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onControlChange
+    (ARG_onControlChange);
+    }}
+void Hooks::onMidiKey::call
+    (PARAM_onMidiKey) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onMidiKey
+    (ARG_onMidiKey);
+    }}
+void Hooks::onMidiCC::call
+    (PARAM_onMidiCC) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onMidiCC
+    (ARG_onMidiCC);
+    }}
+void Hooks::onMidiCCmod::call
+    (PARAM_onMidiCCmod) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onMidiCCmod
+    (ARG_onMidiCCmod);
+    }}
+void Hooks::onMidiCCsustain::call
+    (PARAM_onMidiCCsustain) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onMidiCCsustain
+    (ARG_onMidiCCsustain);
+    }}
+void Hooks::onMidiCCchorus::call
+    (PARAM_onMidiCCchorus) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onMidiCCchorus
+    (ARG_onMidiCCchorus);
+    }}
+void Hooks::onMidiCClegato::call
+    (PARAM_onMidiCClegato) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onMidiCClegato
+    (ARG_onMidiCClegato);
+    }}
+void Hooks::onMidiCCvolume::call
+    (PARAM_onMidiCCvolume) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onMidiCCvolume
+    (ARG_onMidiCCvolume);
+    }}
+void Hooks::onMidiInstrument::call
+    (PARAM_onMidiInstrument) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onMidiInstrument
+    (ARG_onMidiInstrument);
+    }}
+void Hooks::onSerialMessage::call
+    (PARAM_onSerialMessage) 
+    {for (auto& module : hooks.hookModules[hookNum]) {module->
+    onSerialMessage
+    (ARG_onSerialMessage);
+    }}
 
-void Hooks::controlChange(int panel, int number, int state) {
-    for (auto module : OnControlChange) {
-        module->onControlChange(panel,number,state);
-    }
-}
 
-void Hooks::midiKey(int manual, int key, int velocity) {
-    for (auto module : OnMidiKey) {
-        module->onMidiKey(manual,key,velocity);
-    }
-}
-void Hooks::midiCC(int data1, int data2) {
-    for (auto module : OnMidiCC) {
-        module->onMidiCC(data1,data2);
-    }
-}
-void Hooks::midiCCmod(int data) {
-    for (auto module : OnMidiCCmod) {
-        module->onMidiCCmod(data);
-    }
-}
-void Hooks::midiCCsustain(int data) {
-    for (auto module : OnMidiCCsustain) {
-        module->onMidiCCsustain(data);
-    }
-}
-void Hooks::midiCCchorus(int data) {
-    for (auto module : OnMidiCCchorus) {
-        module->onMidiCCchorus(data);
-    }
-}
-void Hooks::midiCClegato(int data) {
-    for (auto module : OnMidiCClegato) {
-        module->onMidiCClegato(data);
-    }
-}
-void Hooks::midiCCvolume(int data) {
-    for (auto module : OnMidiCCvolume) {
-        module->onMidiCCvolume(data);
-    }
-}
-void Hooks::midiInstrument(int instrument) {
-    for (auto module : OnMidiInstrument) {
-        module->onMidiInstrument(instrument);
-    }
-}
-
-void Hooks::serialMessage(std::string target, std::string setting, int data) {
-    for (auto module : OnSerialMessage) {
-        module->onSerialMessage(target,setting,data);
-    }
-}
-
-
-Hooks::Hooks() {
+Hooks::Hooks() : 
+        OnStart(),
+        OnLoop(),
+        OnKeyMove(),
+        OnUserKeyToggle(),
+        OnSystemKeyToggle(),
+        OnUserStopToggle(),
+        OnSystemStopToggle(),
+        OnPedalToggle(),
+        OnControlChange(),
+        OnMidiKey(),
+        OnMidiCC(),
+        OnMidiCCmod(),
+        OnMidiCCsustain(),
+        OnMidiCCchorus(),
+        OnMidiCClegato(),
+        OnMidiCCvolume(),
+        OnMidiInstrument(),
+        OnSerialMessage()
+    {
     console.section("hooks",CORE_PREFIX);
+    isSetup = true;
     
     
     
     console.sectionEnd("hooks initialised",CORE_PREFIX);
 }
+    
+
+void Hooks::setup() {
+    if (!isSetup) {
+       console.section("hooks",CORE_PREFIX);
+        onStart            OnStart;
+        onLoop             OnLoop;
+        onKeyMove          OnKeyMove;
+        onUserKeyToggle    OnUserKeyToggle;
+        onSystemKeyToggle  OnSystemKeyToggle;
+        onUserStopToggle   OnUserStopToggle;
+        onSystemStopToggle OnSystemStopToggle;
+        onPedalToggle      OnPedalToggle;
+        onControlChange    OnControlChange;
+        onMidiKey          OnMidiKey;
+        onMidiCC           OnMidiCC;
+        onMidiCCmod        OnMidiCCmod;
+        onMidiCCsustain    OnMidiCCsustain;
+        onMidiCCchorus     OnMidiCCchorus;
+        onMidiCClegato     OnMidiCClegato;
+        onMidiCCvolume     OnMidiCCvolume;
+        onMidiInstrument   OnMidiInstrument;
+        onSerialMessage    OnSerialMessage;   
+        isSetup = true; 
+        console.sectionEnd("hooks initialised",CORE_PREFIX);
+    }
+    
+    
+    
+    
+    
+}
 
 
-
-
-
-Hooks hook;
+Hooks hooks;
 
 
--- moduleManager.h
+++ moduleManager.h
@@ -8,64 +8,130 @@ using namespace std;
 
 //void keypressHandler(int keyNumber, int state);
 
+/* ----------------------------- HOOK PARAMETERS ---------------------------- */
+#define PARAM_onStart           
+#define PARAM_onLoop            
+#define PARAM_onKeyMove          int key   , int    newPos, int oldPos
+#define PARAM_onUserKeyToggle    int manual, int    key   , int velocity, bool state, int pos
+#define PARAM_onSystemKeyToggle  int manual, int    key   , int velocity, bool state
+#define PARAM_onUserStopToggle   int stop  , bool   state
+#define PARAM_onSystemStopToggle int stop  , bool   state
+#define PARAM_onPedalToggle      int pedal , bool   state
+#define PARAM_onControlChange    int panel , int number, int state
+#define PARAM_onMidiKey          int manual, int key, int velocity
+#define PARAM_onMidiCC           int data1 , int data2
+#define PARAM_onMidiCCmod        int data
+#define PARAM_onMidiCCsustain    int data
+#define PARAM_onMidiCCchorus     int data
+#define PARAM_onMidiCClegato     int data
+#define PARAM_onMidiCCvolume     int data
+#define PARAM_onMidiInstrument   int instrument
+#define PARAM_onSerialMessage    std::string target, std::string setting, int data
+
+/* ----------------------------- HOOK ARGUMENTS ----------------------------- */
+#define ARG_onStart           
+#define ARG_onLoop            
+#define ARG_onKeyMove           key   ,  newPos,  oldPos
+#define ARG_onUserKeyToggle     manual,  key   ,  velocity,  state,  pos
+#define ARG_onSystemKeyToggle   manual,  key   ,  velocity,  state
+#define ARG_onUserStopToggle    stop  ,  state
+#define ARG_onSystemStopToggle  stop  ,  state
+#define ARG_onPedalToggle       pedal ,  state
+#define ARG_onControlChange     panel ,  number,  state
+#define ARG_onMidiKey           manual,  key,     velocity
+#define ARG_onMidiCC            data1 ,  data2
+#define ARG_onMidiCCmod         data
+#define ARG_onMidiCCsustain     data
+#define ARG_onMidiCCchorus      data
+#define ARG_onMidiCClegato      data
+#define ARG_onMidiCCvolume      data
+#define ARG_onMidiInstrument    instrument
+#define ARG_onSerialMessage     target,  setting,  data
+
+
 class moduleTemplate { // This base class contains placeholder functions called at various points during runtime
 public:
-    void onStart();
-    void onLoop();
+    
 
-    void onKeyMove         (int key   , int    newPos, int oldPos);
-    void onUserKeyToggle   (int manual, int    key   , int velocity, bool state, int pos);
-    void onSystemKeyToggle (int manual, int    key   , int velocity, bool state);
-    void onUserStopToggle  (int stop  , bool   state);
-    void onSystemStopToggle(int stop  , bool   state);
-    void onPedalToggle     (int pedal , bool   state);
-    void onControlChange   (int panel , int number, int state);
-
-    void onMidiKey         (int manual, int key, int velocity);
-    void onMidiCC          (int data1 , int data2);
-    void onMidiCCmod       (int data);
-    void onMidiCCsustain   (int data);
-    void onMidiCCchorus    (int data);
-    void onMidiCClegato    (int data);
-    void onMidiCCvolume    (int data);
-    void onMidiInstrument  (int instrument);
-
-    void onSerialMessage(std::string module, std::string setting, int data);
+    virtual void onStart            (PARAM_onStart           );
+    virtual void onLoop             (PARAM_onLoop            );
+    virtual void onKeyMove          (PARAM_onKeyMove         );
+    virtual void onUserKeyToggle    (PARAM_onUserKeyToggle   );
+    virtual void onSystemKeyToggle  (PARAM_onSystemKeyToggle );
+    virtual void onUserStopToggle   (PARAM_onUserStopToggle  );
+    virtual void onSystemStopToggle (PARAM_onSystemStopToggle);
+    virtual void onPedalToggle      (PARAM_onPedalToggle     );
+    virtual void onControlChange    (PARAM_onControlChange   );
+    virtual void onMidiKey          (PARAM_onMidiKey         );
+    virtual void onMidiCC           (PARAM_onMidiCC          );
+    virtual void onMidiCCmod        (PARAM_onMidiCCmod       );
+    virtual void onMidiCCsustain    (PARAM_onMidiCCsustain   );
+    virtual void onMidiCCchorus     (PARAM_onMidiCCchorus    );
+    virtual void onMidiCClegato     (PARAM_onMidiCClegato    );
+    virtual void onMidiCCvolume     (PARAM_onMidiCCvolume    );
+    virtual void onMidiInstrument   (PARAM_onMidiInstrument  );
+    virtual void onSerialMessage    (PARAM_onSerialMessage   );
 
 
 };
 
 
 class Hooks { // This struct contains the vectors of references to objects for each trigger point
+    class hookTemplate {
+    public:
+        hookTemplate();
+        int hookNum;
+        int add(moduleTemplate* module);
+        bool activate(moduleTemplate* module, int pos);
+        bool deactivate(int pos);
+    };
+    bool isSetup = false;
 public:
-    std::vector<moduleTemplate*> OnStart;               void start();
-    std::vector<moduleTemplate*> OnLoop;                void loop();
+    void setup();
+    int numHooks=0; // increments each time a new hook is initialised
+    std::vector<std::vector<moduleTemplate*>> hookModules; // stores pointers to the actual modules
+
+    class onStart            : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onStart           );};
+    class onLoop             : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onLoop            );};
+    class onKeyMove          : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onKeyMove         );};
+    class onUserKeyToggle    : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onUserKeyToggle   );};
+    class onSystemKeyToggle  : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onSystemKeyToggle );};
+    class onUserStopToggle   : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onUserStopToggle  );};
+    class onSystemStopToggle : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onSystemStopToggle);};
+    class onPedalToggle      : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onPedalToggle     );};
+    class onControlChange    : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onControlChange   );};
+    class onMidiKey          : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onMidiKey         );};
+    class onMidiCC           : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onMidiCC          );};
+    class onMidiCCmod        : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onMidiCCmod       );};
+    class onMidiCCsustain    : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onMidiCCsustain   );};
+    class onMidiCCchorus     : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onMidiCCchorus    );};
+    class onMidiCClegato     : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onMidiCClegato    );};
+    class onMidiCCvolume     : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onMidiCCvolume    );};
+    class onMidiInstrument   : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onMidiInstrument  );};
+    class onSerialMessage    : public hookTemplate{public: using hookTemplate::hookTemplate; void call (PARAM_onSerialMessage   );};
     
-    std::vector<moduleTemplate*> OnKeyMove;             void keyMove(int key, int newPos, int oldPos);
-    std::vector<moduleTemplate*> OnUserKeyToggle;       void userKeyToggle(int manual, int key, int velocity, bool state, int pos);
-    std::vector<moduleTemplate*> OnSystemKeyToggle;     void systemKeyToggle(int manual, int key, int velocity, bool state);
-
-    std::vector<moduleTemplate*> OnUserStopToggle;      void userStopToggle(int stop, bool state);
-    std::vector<moduleTemplate*> OnSystemStopToggle;    void systemStopToggle(int stop, bool state);
-
-    std::vector<moduleTemplate*> OnPedalToggle;         void pedalToggle(int pedal, bool state);
-
-    std::vector<moduleTemplate*> OnControlChange;       void controlChange(int panel, int number, int state);
-
-    std::vector<moduleTemplate*> OnMidiKey;             void midiKey(int manual, int key, int velocity);
-    std::vector<moduleTemplate*> OnMidiCC;              void midiCC(int data1, int data2);
-    std::vector<moduleTemplate*> OnMidiCCmod;           void midiCCmod(int data);
-    std::vector<moduleTemplate*> OnMidiCCsustain;       void midiCCsustain(int data);
-    std::vector<moduleTemplate*> OnMidiCCchorus;        void midiCCchorus(int data);
-    std::vector<moduleTemplate*> OnMidiCClegato;        void midiCClegato(int data);
-    std::vector<moduleTemplate*> OnMidiCCvolume;        void midiCCvolume(int data);
-    std::vector<moduleTemplate*> OnMidiInstrument;      void midiInstrument(int instrument);
-
-    std::vector<moduleTemplate*> OnSerialMessage;       void serialMessage(string module, string setting, int data);
-
+    onStart             OnStart;             
+    onLoop              OnLoop;            
+    onKeyMove           OnKeyMove;         
+    onUserKeyToggle     OnUserKeyToggle;   
+    onSystemKeyToggle   OnSystemKeyToggle; 
+    onUserStopToggle    OnUserStopToggle;  
+    onSystemStopToggle  OnSystemStopToggle;
+    onPedalToggle       OnPedalToggle;     
+    onControlChange     OnControlChange;   
+    onMidiKey           OnMidiKey;         
+    onMidiCC            OnMidiCC;          
+    onMidiCCmod         OnMidiCCmod;       
+    onMidiCCsustain     OnMidiCCsustain;   
+    onMidiCCchorus      OnMidiCCchorus;    
+    onMidiCClegato      OnMidiCClegato;    
+    onMidiCCvolume      OnMidiCCvolume;    
+    onMidiInstrument    OnMidiInstrument;  
+    onSerialMessage     OnSerialMessage;   
+    
     Hooks();
 };
 
-extern Hooks hook;
+extern Hooks hooks;
 
 #endif
