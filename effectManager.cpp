3c991bb70f7b4e1bd51af179757f0af0969194e4
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-11-22 17:52:54 +0000

    added runFx function, created fxData type

--- effectManager.cpp
+++ effectManager.cpp
@@ -1,5 +1,8 @@
 #include "effectManager.h"
 
+fxData effectTemplate::applyEffect(fxData previousResult){}
+
+
 
 effectManager::effectManager() {
     console.section("effectManager",CORE_PREFIX);
@@ -8,3 +11,32 @@ effectManager::effectManager() {
     
     console.sectionEnd("effectManager initialised",CORE_PREFIX);
 }
+
+void effectManager::addEffect(effectTemplate* effect) {
+    totalNumFx +=1;
+    
+    
+}
+
+
+void effectManager::runFx(int manual, int key, int state) {
+    if (!activeFx.empty()){
+        fxData result; // stores the result after each effect applied
+        result.emplace_back(manual,key,state,0); // adds the first keypress to the list
+        for (int i = 0; i < totalNumFx; i++) {
+            result = allFx[i]->applyEffect(result);
+        }
+        for (auto & keypress : result) {        
+            stateManager.keys.requestActuatorState(
+                std::get<0>(keypress),
+                std::get<1>(keypress),
+                std::get<2>(keypress),
+                std::get<3>(keypress)
+            );
+            
+        }
+    }
+    
+}
+
+effectManager Fx;
--- effectManager.h
+++ effectManager.h
@@ -2,24 +2,27 @@
 #define EFFECT_MANAGER_H
 
 #include "global_includes.h"
+#include "stateManager.h"
 
+typedef std::vector<std::tuple<int,int,int,unsigned long>> fxData;
 
 class effectTemplate {
-private:
-    std::vector<std::tuple<int,int,int,unsigned long>> output;
+protected:
+    int fxIndex;
+    fxData output;
 public:
     
     friend class effectManager; // the effectManager is a friend so can access the private variables
     bool skip; // whether the result of this one should be skipped
-    std::vector<int> applyEffect(std::vector<int> original, std::vector<std::tuple<int,int,int,unsigned long>> );
+    virtual fxData applyEffect(fxData previousResult);
 
 };
 
 
 class effectManager {
     std::vector<effectTemplate*> allFx; // pointers to all effects available
-    std::deque<effectTemplate*> activeFx; // stores pointers to all active effects
-    
+    std::deque<int> activeFx; // stores pointers to all active effects
+    int totalNumFx=0;
     
 public:
     effectManager();
@@ -34,9 +37,11 @@ public:
     void removeActive(int index);
     void popBackActive();
     void popFrontActive();
+    
+    void runFx(int manual, int key, int state);
 };
 
-effectManager Effects;
+extern effectManager Fx;
 
 
 
