0bd5bf95c5c5f452922e8492f0cee923d8a75224
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-12-19 13:52:44 +0000

    improved efficiency, set pullups seperately

--- multiplexer.h
+++ multiplexer.h
@@ -4,31 +4,40 @@
 #include "global_includes.h"
 #include <ADC.h>
 
+
 class analogManager {
     public:
-    ADC *adc = new ADC();
+    const ADC *adc = new ADC();
     analogManager();
 };
-extern analogManager analog_manager;
+extern analogManager * analog_manager;
 
-// Takes the I/O number and configures the mux to switch to the correct pin, then reads / outputs to it. constructor:(if an input mux,  no. of muxes used in this I/O array, mux select pins (vector, ordered, 4 items), I/O pin for each mux (vector, ordered) )
+// Instantiated for each chain of multiplexers connected to the control board. Can be used as input or output (though should use I2C port expanders for digital input or output)
 class Multiplexer {
-    std::vector<int> selectPins = MUX_SELECT_PINS;
-    std::vector<int> enablePins = MUX_ENABLE_PINS;
+
+    
+    int selectPins[4] = MUX_SELECT_PINS;
+    int enablePins[4] = MUX_ENABLE_PINS;
+    
     int numOfMux; 
     int IOpin;
 
     bool input;
-    bool pullup;
+    //bool pullup;
     bool continuous;
-    void writeBinary(std::vector<int>& pins, int value);
-    void setEnablePins(int mux);
+    
+    std::vector<bool> pullups;
+    
+    void writeBinary(int pins[4], int value);
+    void prepare(int pin);
 
 public:
     Multiplexer(bool _input, bool _continuous, int number, int _IOpin);
-
-    int muxRead(int index, bool analog, bool pullup, int microsecondDelay);
-
+    
+    void selectMuxAndPin(int index);
+    void setPullup(int pin, bool pullup);
+    int AnalogReadSingle(int pin);
+    int DigitalRead(int pin);
 };
 
 
