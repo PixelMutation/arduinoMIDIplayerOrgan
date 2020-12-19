684c5718c0ba775b5e9187c5e90c20d51765fbbd
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-12-19 13:51:23 +0000

    various methods defined

--- effectManager.cpp
+++ effectManager.cpp
@@ -1,37 +1,121 @@
 #include "effectManager.h"
 
-fxData effectTemplate::applyEffect(fxData previousResult){}
+#include "stateManager.h"
+#include "console.h"
+#include "moduleManager.h"
+
+
+// applies an effect to the keypress and output of previous effects
+fxData fxTemplate::applyEffect(fxData previousResult){
+    return previousResult;
+}
 
 
 
 effectManager::effectManager() {
-    console.section("effectManager",CORE_PREFIX);
+    console->section("effectManager",CORE_PREFIX);
     
+    console->println((unsigned int)fxNames.size());
     
-    
-    console.sectionEnd("effectManager initialised",CORE_PREFIX);
+    console->sectionEnd("effectManager initialised",CORE_PREFIX);
 }
 
-void effectManager::addEffect(effectTemplate* effect) {
-    totalNumFx +=1;
-    
+void effectManager::addEffect(fx effect, std::string name) {
+    if (listFind(effect,activeFx)==-1) {
+        activeFx.push_back(effect);
+        fxNames.push_back(name);
+        console->println("addedEffect");
+        console->println((unsigned int)activeFx.size());
+        console->println((unsigned int)fxNames.size());
+        totalNumFx+=1;
+    }
+}
+void effectManager::moveBack(fx effect) {
+    move(effect,totalNumFx-1);
+}
+void effectManager::moveBackwards(fx effect) {
+    int index = listFind(effect,activeFx)-1;
+    if (index>=0) {
+        move(effect,index);
+    }
     
 }
+void effectManager::moveFront(fx effect) {
+    move(effect,0);
+}
+void effectManager::moveForwards(fx effect) {
+    int index = listFind(effect,activeFx)+1;
+    if (index<totalNumFx) {
+        move(effect,index);
+    }
+    
+}
+inline void effectManager::move(fx effect, int index) {
+    int oldIndex = listFind(effect,activeFx);
+    if (oldIndex!=-1) {
+        if (index < totalNumFx) {
+            fx currentFx = activeFx[index]; // the effect already at that position
+            std::string currentName = fxNames[index];
+            fx newFx = activeFx[oldIndex]; // the effect already at that position
+            std::string newName = fxNames[oldIndex];
+            
+            activeFx[index] = newFx;
+            fxNames[index] = newName;
+            
+            activeFx[oldIndex] = currentFx;
+            fxNames[oldIndex] = currentName;
+        }
 
+        
+    } 
+    
+}
+std::string effectManager::getName(fx effect) {
+    int index = listFind(effect,activeFx)-1;
+    if (index!=-1) {
+        return fxNames[listFind(effect,activeFx)];
+    }
+    else {
+        return "INVALID";
+    }
+}
+ void effectManager::printEffectOrder() {
+    console->printPrefix("FX  : ");
+    for (auto name : fxNames) {
+       console->print(name);
+       console->print("");
+    }
+ }
 
+// runs through all active effects (checks if fxActive==true)
 void effectManager::runFx(int manual, int key, int state) {
+    if (state == 0) {
+        console->println("runFx 0");
+    } else {
+        console->println("runFx");
+    }
+    
     if (!activeFx.empty()){
+        console->println("foundFx");
         fxData result; // stores the result after each effect applied
         result.emplace_back(manual,key,state,0); // adds the first keypress to the list
         for (int i = 0; i < totalNumFx; i++) {
-            result = allFx[i]->applyEffect(result);
+            if (activeFx[i]->fxActive) { // only runs effect when active
+                result = activeFx[i]->applyEffect(result); // runs each effect, feeding the next one the result of the previous
+            }
         }
-        for (auto & keypress : result) {        
-            stateManager.keys.requestActuatorState(
-                std::get<0>(keypress),
-                std::get<1>(keypress),
-                std::get<2>(keypress),
-                std::get<3>(keypress)
+        for (int i = 1; i < (int)result.size(); i++) {  // requests actuation of all resulting keypresses except the first      
+            stateManager->keys.requestActuatorState(
+                std::get<0>(result[i]),
+                std::get<1>(result[i]),
+                std::get<2>(result[i]),
+                std::get<3>(result[i])
+            );
+            hooks->OnSystemKeyToggle->call(
+                std::get<0>(result[i]),
+                std::get<1>(result[i]),
+                std::get<2>(result[i]),
+                std::get<3>(result[i])
             );
             
         }
@@ -39,4 +123,4 @@ void effectManager::runFx(int manual, int key, int state) {
     
 }
 
-effectManager Fx;
+effectManager * Fx = nullptr;
--- effectManager.h
+++ effectManager.h
@@ -2,46 +2,45 @@
 #define EFFECT_MANAGER_H
 
 #include "global_includes.h"
-#include "stateManager.h"
 
-typedef std::vector<std::tuple<int,int,int,unsigned long>> fxData;
 
-class effectTemplate {
-protected:
-    int fxIndex;
-    fxData output;
+
+using fxData = std::vector<std::tuple<int,int,int,unsigned long>>;
+
+class fxTemplate {
 public:
-    
-    friend class effectManager; // the effectManager is a friend so can access the private variables
-    bool skip; // whether the result of this one should be skipped
+    bool fxActive = false;
     virtual fxData applyEffect(fxData previousResult);
 
 };
-
+using fx = fxTemplate*;
 
 class effectManager {
-    std::vector<effectTemplate*> allFx; // pointers to all effects available
-    std::deque<int> activeFx; // stores pointers to all active effects
+    
+    std::deque<fx> activeFx; // stores pointers to all active effects
+    std::deque<std::string> fxNames;
     int totalNumFx=0;
     
 public:
     effectManager();
 
 
-    void addEffect(effectTemplate* effect); // adds new effect to master list
+    void addEffect(fx effect, std::string name); // adds new effect to master list
     int numActiveFx();
 
-    void pushBackActive(effectTemplate* effect); // adds effect to end of list
-    void pushFrontActive(effectTemplate* effect); // adds effect to start of list
-    void insertActive(effectTemplate* effect,int pos);    //adds effect at a particular point (the end if invalid index given)
-    void removeActive(int index);
-    void popBackActive();
-    void popFrontActive();
+    void moveBack(fx effect); // moves effect to end of list
+    void moveBackwards(fx effect);
+    void moveFront(fx effect); // moves effect to start of list
+    void moveForwards(fx effect);
+    void move(fx effect,int index);    //adds effect at a particular point (the end if invalid index given)
+    
+    std::string getName(fx effect);
+    void printEffectOrder();
     
     void runFx(int manual, int key, int state);
 };
 
-extern effectManager Fx;
+extern effectManager * Fx;
 
 
 
