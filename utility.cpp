12201261ade546ce438ab75a11f278833b27f996
Charlie Spencer <charlie.e.spencer@gmail.com>
2020-12-19 13:54:01 +0000

    removed printVector, added vector find

--- utility.cpp
+++ utility.cpp
@@ -2,54 +2,7 @@
 
 
 
-void printvector(std::vector<int> vector, bool displayZero) {
-	for (int element : vector) {
 
-		if (element != 0) {
-			Serial.print(" ");
-			Serial.print(element);
-
-		}
-		else {
-			if (displayZero) {
-				Serial.print(" ");
-				Serial.print(element);
-			}
-			else {
-				Serial.print("  ");
-			}
-		}
-		
-
-
-		Serial.print(" ");
-	}
-
-}
-
-void printvector(std::deque<double> vector, bool displayZero) {
-	for (double element : vector) {
-
-		if (element != 0) {
-			Serial.print(" ");
-			Serial.print(element);
-		}
-		else {
-			if (displayZero) {
-				Serial.print(" ");
-				Serial.print(element);
-			}
-			else {
-				Serial.print("  ");
-			}
-		}
-
-
-
-		Serial.print(" ");
-	}
-
-}
 // returns whether a value is above or below a threshold, taking into account a deadzone
 int thresholdCheck(int val, int threshold, int deadzone) {
     if (val < threshold-deadzone/2) {
--- utility.h
+++ utility.h
@@ -3,12 +3,24 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
-#include "global_includes.h"
+#include "configuration.h"
+#include "console.h"
+#include <algorithm>
 
 
 
-void printVector(std::vector<int   > vector, bool displayZero = true);
-void printVector(std::deque <double> vector, bool displayZero = true);
+
+template<typename T,typename T2>
+int listFind(T & element,T2 & list) {
+    auto pos = std::find(list.begin(),list.end(),element);
+    if (pos != list.end()){
+        return std::distance(list.begin(),pos);
+    } else {
+        return -1;
+    }
+}
+
+
 
 int thresholdCheck(int val, int threshold, int deadzone=POSITION_DEADZONE);
 
